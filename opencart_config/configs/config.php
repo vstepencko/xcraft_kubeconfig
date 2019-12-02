08dc1a14311fd3aed7c77156f28905fd098c8d6d
vstepencko <vstepencko@gmail.com>
2019-12-02 16:51:41 +0200

    Opencart dockerfile added

---
+++ opencart_config/configs/config.php
@@ -0,0 +1,25 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'https://xcraft.com.ua/');
+// HTTPS
+define('HTTPS_SERVER', 'https://xcraft.com.ua/');
+// DIR
+define('DIR_APPLICATION', '/var/www/html/catalog/');
+define('DIR_SYSTEM', '/var/www/html/system/');
+define('DIR_IMAGE', '/var/www/html/image/');
+define('DIR_LANGUAGE', '/var/www/html/catalog/language/');
+define('DIR_TEMPLATE', '/var/www/html/catalog/view/theme/');
+define('DIR_CONFIG', '/var/www/html/system/config/');
+define('DIR_CACHE', '/var/www/html/system/storage/cache/');
+define('DIR_DOWNLOAD', '/var/www/html/system/storage/download/');
+define('DIR_LOGS', '/var/www/html/system/storage/logs/');
+define('DIR_MODIFICATION', '/var/www/html/system/storage/modification/');
+define('DIR_UPLOAD', '/var/www/html/system/storage/upload/');
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'db');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'karatau');
+define('DB_DATABASE', 'xcraftco_corp');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
